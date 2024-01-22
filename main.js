6a2a0f3db1dabf946a010b7a52fbb049bd8676d8
José Alan <104922063+Alanz0ka@users.noreply.github.com>
2024-01-22 14:00:48 -0300

    Create main.js

---
+++ main.js
@@ -0,0 +1,14 @@
+const Jogo = require('./Jogo');
+
+const jogo = new Jogo();
+console.log('Bem-vindo ao Jogo de Adivinhação de Palavras Estilizado!');
+console.log(`Palavra: ${jogo.palavraSelecionada.getPalavraAdivinhada()}`);
+
+while (!jogo.palavraSelecionada.isPalavraAdivinhada() && jogo.tentativasRestantes > 0) {
+  jogo.tentarLetra();
+}
+
+if (!jogo.palavraSelecionada.isPalavraAdivinhada()) {
+  console.log('Você perdeu! A palavra correta era: ' + jogo.palavraSelecionada.palavra);
+  jogo.exibirMensagemFinal('');
+}
